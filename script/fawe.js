e2e44c6987be49cce090c4671ef7c0a897f362bd
Jordan <dordsor21@gmail.com>
2022-04-16 23:20:42 +0100

    Reduce length of versions

--- script/fawe.js
+++ script/fawe.js
@@ -9,7 +9,7 @@ var heightmap_interface = {
 }
 
 var projects = {
-	"Spigot 1.18.2, 1.18.1, 1.18, 1.17.1, 1.16.5": {
+	"Spigot 1.16.5-1.18.2": {
 		"download": [
 			{
 				"label": "Download FAWE",
